0c9d0471b29b5b1729aced5d35f444acf789d11f
Marc Hayek <mhayek.90@gmail.com>
2015-06-15 00:10:51 +0300

    First Commit

---
+++ content.js
@@ -0,0 +1,26 @@
+new function() {
+	jQuery.fn.center = function () {
+	    this.css("position","fixed");
+	    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+	                                                $(window).scrollTop()) + "px");
+	    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+	                                                $(window).scrollLeft()) + "px");
+	    return this;
+	}
+	var soundUrl= chrome.extension.getURL('glass.mp3');
+	var sound = new Audio(soundUrl); // buffers automatically when created
+	
+	var url = chrome.extension.getURL('img/crack.png');
+		window.addEventListener('error', function(e) {
+			if(e.filename) {
+				if(e.type=="error"){
+					
+					var $newdiv1 = $( "<div class='domthingy'><img src="+url+"></div>" );
+
+				 	$("body").append($newdiv1);
+				 	$(".domthingy").center();
+				 	sound.play();
+				}
+			}
+		});
+};
